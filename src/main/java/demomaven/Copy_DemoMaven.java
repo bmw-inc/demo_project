ededf5c65a699f5f63d135192fec956381179872
Always Owner <117433443+alwaysowner007@users.noreply.github.com>
2023-03-21 17:09:48 +0530

    Revised

--- src/main/java/demomaven/Copy_DemoMaven.java
+++ src/main/java/demomaven/Copy_DemoMaven.java
@@ -2,39 +2,26 @@ package demomaven;
 
 public class Copy_DemoMaven {
 
-	public static void main(String[] args) {
-		int k;
-	}
-	
-	public boolean unusedMethod(boolean k) {
-		k = true;
-		
+    public static void main(String[] args) {
+        int i;
+    }
+
+    public boolean unusedMethod(boolean x) {
+        x = true;
+
         String foo = null;
         System.out.println(foo.length());
-        
-		return k;
-	}
-	
-	public boolean unusedMethod2(boolean t) {
-		t = true;
-		
+
+        return x;
+    }
+
+    public boolean unusedMethod2(boolean y) {
+        y = true;
+
         String foo = null;
         System.out.println(foo.length());
-        
-		return t;
-	}
-	
-	public boolean unusedMethod3(boolean j) {
-		j = true;
-		
-        String foo = null;
-        System.out.println(foo.length());
-        
-		return j;
-	}
-	
-	public boolean unusedLocalVar(boolean i) {
-		int h;
-	}
+
+        return y;
+    }
 
 }
