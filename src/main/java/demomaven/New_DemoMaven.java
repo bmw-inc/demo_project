10d688e58a786e5921b1a962cec556262b0f8408
Always Owner <117433443+alwaysowner007@users.noreply.github.com>
2023-03-28 10:35:39 +0530

    Create New_DemoMaven.java

---
+++ src/main/java/demomaven/New_DemoMaven.java
@@ -0,0 +1,25 @@
+package demomaven;
+
+public class New_DemoMaven {
+
+	public static void main(String[] args) {
+		int k;
+	}
+	
+	public boolean unusedMethod(boolean k) {
+		k = true;
+		
+        
+		return k;
+	}
+	
+	public boolean unusedMethod2(boolean k) {
+		boolean p=true;
+		return p;
+	}
+	
+	public boolean unusedMethod3() {
+	    int z;
+	}
+
+}
